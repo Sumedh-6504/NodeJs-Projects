e6781fbdfbcf1c874da92726207df0e69f945b31
Sumedh-6504 <sumedhrmundewadi@gmail.com>
2025-01-05 15:06:03 +0530

    changed some code

--- paytm/backend/routes/users.js
+++ paytm/backend/routes/users.js
@@ -1,4 +1,32 @@
 import express from "express";
+import { createUser } from "../routes/validations";
 const router = express.Router();
+
+router.post("/signup", async (req, res, next) => {
+  const createdPayload = req.body();
+  const parsedPayload = createUser.safeParse(createdPayload);
+
+  if (!parsedPayload.success) {
+    res
+      .status(411)
+      .json({ message: "The credentials given by the user were not valid!" });
+  } else {
+    try {
+      await User.create({
+        userName: createdPayload.userName,
+        firstName: createdPayload.firstName,
+        lastName: createdPayload.lastName,
+        password: createdPayload.password,
+      });
+      res.json({
+        msg: "User created successfully!",
+      });
+    } catch (err) {
+      res.status(411).json({
+        msg: `There was an error as ${err}`,
+      });
+    }
+  }
+});
 
 module.exports = router;
