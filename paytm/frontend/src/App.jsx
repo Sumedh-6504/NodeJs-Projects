a8dcf27b5e304a2d8d6cdf2288719c2f0436cbe9
Sumedh-6504 <sumedhrmundewadi@gmail.com>
2025-02-16 00:15:35 +0530

    changed

--- paytm/frontend/src/App.jsx
+++ paytm/frontend/src/App.jsx
@@ -1,16 +1,14 @@
-
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/signup" element={<Signup />}/>
-      <Route path="/signin" element={<Signin/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route path="/send" element={<Send/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Send />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
