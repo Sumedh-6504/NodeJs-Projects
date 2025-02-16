b350eb34d383c074e3e28ef59aaabe4ce202c0a8
Sumedh-6504 <sumedhrmundewadi@gmail.com>
2025-02-16 21:08:03 +0530

    Changes made

---
+++ paytm/frontend/pages/Dashboard.jsx
@@ -0,0 +1,4 @@
+export function Dashboard() {
+  // const []
+  return <div>Hello World!</div>;
+}
--- paytm/frontend/src/App.jsx
+++ paytm/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Signup, Signin, Dashboard, Send } from "./pages";
+import { Signup, Signin } from "../pages";
 
 function App() {
   return (
@@ -8,8 +8,8 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/send" element={<Send />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Send />} /> */}
       </Routes>
     </BrowserRouter>
   );
