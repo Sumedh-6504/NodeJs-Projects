60c315c57ca209353dd822aa6fc5592ad3941029
Sumedh-6504 <sumedhrmundewadi@gmail.com>
2025-02-17 23:28:35 +0530

    changes made

--- paytm/frontend/components/InputBox.jsx
+++ paytm/frontend/components/InputBox.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
-export function InputBox({ label, placeholder }) {
+export function InputBox({ label, placeholder, onChange }) {
   return (
     <div>
       <div className="text-sm font-medium text-left py-2">{label}</div>
       <input
-        type="text"
+        onChange={onChange}
         placeholder={placeholder}
         className="w-full px-2 py-1 border rounded-border-slate-200"
       />
